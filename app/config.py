365239e9c1d394ce6fadb686567b5553c5864e25
vinaykontham <konthamvinay4@gmail.com>
2025-03-11 16:52:56 +0530

    update

--- app/config.py
+++ app/config.py
@@ -1,5 +1,11 @@
 from fastapi import APIRouter
 from app.auth import router as auth_router
+import os
+from dotenv import load_dotenv
+# Load environment variables from .env file
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@db:5432/urls")
 
 router = APIRouter()
 
