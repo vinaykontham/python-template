78e274dd70b06173d822683205ea5d00a463c942
vinaykontham <konthamvinay4@gmail.com>
2025-03-11 21:51:58 +0530

    update

--- app/database.py
+++ app/database.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 from app.config import DATABASE_URL
 
 # Create a database engine
 engine = create_engine(DATABASE_URL)
-Base = declarative_base()
+Base = declarative_base() 
 # Create a session factory
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
