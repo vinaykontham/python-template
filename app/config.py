1c9e45ce981ed2aa835b0dd8f44664a8e78c9208
vinaykontham <konthamvinay4@gmail.com>
2025-03-11 21:58:36 +0530

    update

--- app/config.py
+++ app/config.py
@@ -4,8 +4,9 @@ import os
 from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
+IS_TEST_ENV = os.getenv("PYTHON_ENV") == "test"
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@db:5432/urls")
+DATABASE_URL = "sqlite:///./test.db" if IS_TEST_ENV else os.getenv("DATABASE_URL", "postgresql://user:password@db:5432/urls")
 
 router = APIRouter()
 
